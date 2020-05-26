9799414c03ada849e8a84c882073fb9185a58b0a
Knight Radiant <45047039+artfulracoon@users.noreply.github.com>
2020-05-26 15:24:16 +0300

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -315,6 +315,7 @@ public class TicTacToe {
     }
 
     public static void main(String[] args) {
+        System.out.print("\033[H\033[2J");
         rulesPrinter();
         adSetter();
         while (chooser == 1) {
